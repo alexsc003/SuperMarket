10a99e9507fff11e38b5ea17fc2cf7850a2e9eaf
alexsc003 <145650775+alexsc003@users.noreply.github.com>
2024-11-14 13:15:44 +0100

    Destruccion
    DESTRUCION NO FUNCIONA JFDSASDBASDFBSADCASCN

--- Assets/Scrpts/SuperMerfcado.cs
+++ Assets/Scrpts/SuperMerfcado.cs
@@ -6,8 +6,12 @@ public class SuperMerfcado : MonoBehaviour
 {
     [SerializeField]
     GameObject[] muebles;
+    [SerializeField]
+    GameObject objectoSeleccionado;
 
     GameObject objectoCreado;
+    [SerializeField]
+    GameObject modoDestruccion;
 
     [SerializeField]
     GameObject popUpAcciones;
@@ -22,6 +26,8 @@ public class SuperMerfcado : MonoBehaviour
 
     public int i;
 
+    public float speedScale = 3.0f;
+
     public void ElegirMueble(int amount)
     {
         i = amount;
@@ -32,24 +38,23 @@ public class SuperMerfcado : MonoBehaviour
 
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-
-        if (Input.GetKey(KeyCode.Q))
-        {
-            objectoCreado.transform.Rotate(0.0f, -0.5f, 0.0f);
-        }
-        if (Input.GetKey(KeyCode.E))
-        {
-            objectoCreado.transform.Rotate(0.0f, 0.5f, 0.0f);
-        }
-        if (Physics.Raycast(ray, out hit))
-        {
-            objectoCreado.transform.position = hit.point;
-        }
         
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            Destroy(objectoCreado);
+        Destruccion();
+        
+        
+            Vector3 newScale = transform.localScale;
+            newScale += Vector3.one * Input.mouseScrollDelta.y * Time.deltaTime * speedScale;
+            transform.localScale = newScale;
+            objectoCreado.transform.localScale = newScale;
+
+        Rotacion();
+        
+        if (Physics.Raycast(ray, out hit))
+        {                   
+            objectoCreado.transform.position = hit.point;           
         }
+        DestruccionClick();
+
         objectoCreado.SetActive(true);
         if (objectoCreado.activeSelf)
         {
@@ -72,6 +77,44 @@ public class SuperMerfcado : MonoBehaviour
     {
         LeanTween.moveLocal(popUpAcciones, new Vector3(0.0f, alturaActivo, 0.0f), 2f).setEase(LeanTweenType.easeOutElastic);
         LeanTween.moveLocal(popUpMuebles, new Vector3(distanciaDesactivo, 0.0f, 0.0f), 2f).setEase(LeanTweenType.easeOutElastic);
+        modoDestruccion.SetActive(false);
         Destroy(objectoCreado);
+    }
+    public void DestruccionClick()
+    { 
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetMouseButtonDown(1))
+        {
+            Destroy(objectoCreado);
+        }
+    }
+    public void Rotacion()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            objectoCreado.transform.Rotate(0.0f, -0.5f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            objectoCreado.transform.Rotate(0.0f, 0.5f, 0.0f);
+        }
+    }
+    public void Destruccion()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                    Debug.Log("Clicked");
+                    Destroy(hit.transform.gameObject);
+
+            }
+        }
+    }
+    public void BotonDestruccion()
+    {
+        modoDestruccion.SetActive(true);
+        LeanTween.moveLocal(popUpAcciones, new Vector3(0.0f, alturaDesactivo, 0.0f), 2f).setEase(LeanTweenType.easeOutElastic);
     }
 }

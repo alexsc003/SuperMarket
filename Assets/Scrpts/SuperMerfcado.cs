89f7fee9effceb3f444cf641ed1a298d9b96e033
alexsc003 <alexnebra03@gmail.com>
2024-11-21 12:27:53 +0100

    Entrega con modificaciones

--- Assets/Scrpts/SuperMerfcado.cs
+++ Assets/Scrpts/SuperMerfcado.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class SuperMerfcado : MonoBehaviour
 {
+    float tiempo;
+
     [SerializeField]
     GameObject[] muebles;
-    [SerializeField]
-    GameObject objectoSeleccionado;
 
     GameObject objectoCreado;
     [SerializeField]
@@ -18,8 +18,11 @@ public class SuperMerfcado : MonoBehaviour
 
     [SerializeField]
     GameObject popUpMuebles;
-
-    public float alturaActivo;
+    
+    [SerializeField]
+    GameObject circulo;
+    
+        public float alturaActivo;
     public float distanciaActivo;
     public float alturaDesactivo;
     public float distanciaDesactivo;
@@ -34,15 +37,23 @@ public class SuperMerfcado : MonoBehaviour
     }
     void Update()
     {
+        tiempo = tiempo + Time.deltaTime;
         objectoCreado.SetActive(false);
 
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
+        if(tiempo <= 0.5)
+        { 
+        LeanTween.scale(circulo, new Vector3(19f, 1f, 19f), 0.5f);
+        }
+        if(tiempo >= 1) 
+        { 
+        LeanTween.scale(circulo, new Vector3(16f, 1f, 16f), 0.0f);
+            tiempo = 0;
+        }
+       
         
-        Destruccion();
-        
-        
-            Vector3 newScale = transform.localScale;
+        Vector3 newScale = transform.localScale;
             newScale += Vector3.one * Input.mouseScrollDelta.y * Time.deltaTime * speedScale;
             transform.localScale = newScale;
             objectoCreado.transform.localScale = newScale;
@@ -51,10 +62,11 @@ public class SuperMerfcado : MonoBehaviour
         
         if (Physics.Raycast(ray, out hit))
         {                   
-            objectoCreado.transform.position = hit.point;           
+            objectoCreado.transform.position = hit.point;
+            circulo.transform.position = hit.point;
         }
         DestruccionClick();
-
+        
         objectoCreado.SetActive(true);
         if (objectoCreado.activeSelf)
         {
@@ -67,6 +79,7 @@ public class SuperMerfcado : MonoBehaviour
     public void CrearObjecto()
     {
         objectoCreado = Instantiate(muebles[i], Vector3.zero, Quaternion.identity);
+        circulo.SetActive(true);
     }
     public void PopUpObjectos()
     {
@@ -85,6 +98,7 @@ public class SuperMerfcado : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Escape) || Input.GetMouseButtonDown(1))
         {
             Destroy(objectoCreado);
+            circulo.SetActive(false);
         }
     }
     public void Rotacion()
@@ -117,4 +131,5 @@ public class SuperMerfcado : MonoBehaviour
         modoDestruccion.SetActive(true);
         LeanTween.moveLocal(popUpAcciones, new Vector3(0.0f, alturaDesactivo, 0.0f), 2f).setEase(LeanTweenType.easeOutElastic);
     }
+    
 }
